c046877b45c638e1df8c33249b8263ed272ba5c9
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-10 18:05:06 +0200

    Use the already defined durations as dividers

--- src/lib.js
+++ src/lib.js
@@ -1,13 +1,3 @@
 const parse = require('parse-duration');
 
-let ms, s, m, h, d, w;
-const dividers = {
-  ms: ms = 1,
-  s: s = ms * 1000,
-  m: m = s * 60,
-  h: h = m * 60,
-  d: d = h * 24,
-  w: w = d * 7,
-};
-
-module.exports = (str, format = 'ms') => parse(str) / dividers[format];
+module.exports = (str, format = 'ms') => parse(str) / parse[format];
